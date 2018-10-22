3deb12bbf30daaa3c6079caed5ead9c708e68f06
Jason He <jasonhe233@hotmail.com>
2018-10-22 14:35:24 -0400

    Upload solution to bubble sort

--- Oct22-Bubble-Sort.js
+++ Oct22-Bubble-Sort.js
@@ -1,1 +1,31 @@
+function swap(array, i, j) {
+  let temp = array[i];
+  array[i] = array[j];
+  array[j] = temp;
+}
+
+function bubbleSortLoopy(array) {
+  for(let i = 0; i < array.length; i++) {
+    for(let j = 1; j < array.length; j++) {
+      if(array[j - 1] > array[j]) {
+        swap(array, j - 1, j);
+      }
+    }
+  }
+  return array;
+}
+
+function bubbleSortQuick(array) {
+  let swapped;
+  do {
+    swapped = false;
+    for(let i = 0; i < array.length; i++) {
+      if(array[i + 1] !== undefined && array[i] > array[i + 1]) {
+        swap(array, i, i + 1);
+        swapped = true;
+      }
+    }
+  } while(swapped);
+  return array;
+}
 
