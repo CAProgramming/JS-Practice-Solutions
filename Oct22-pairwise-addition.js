2fd1c6e45185203d087bdc8622c242b9704383cc
wtlo <wesleylo2009@gmail.com>
2018-10-22 19:22:19 -0400

    Update Oct22-pairwise-addition.js

--- Oct22-pairwise-addition.js
+++ Oct22-pairwise-addition.js
@@ -1,21 +1,21 @@
 var arr1 = [12,42,2,1,2];
+
 function pairwise_addition(array1,array2,shorten) {
 	var arrayAdded = []
-  
-  for (var x = 0; x < Math.max(array1.length,array2.length); x ++) {
-    if (x < Math.min(array1.length,array2.length)) {
-      arrayAdded.push(array1[x] + array2[x]);
-    } else if (!shorten) {
-    	if (array1.length > array2.length) {
-    		arrayAdded.push(array1[x]);
-      } else {
-    		arrayAdded.push(array2[x]);
-      }
-    }
-    
-    }
-    return arrayAdded;
-  
+
+	for (var x = 0; x < Math.max(array1.length,array2.length); x ++) {
+		if (x < Math.min(array1.length,array2.length)) {
+			arrayAdded.push(array1[x] + array2[x]);
+		} else if (!shorten) {
+			if (array1.length > array2.length) {
+				arrayAdded.push(array1[x]);
+			} else {
+				arrayAdded.push(array2[x]);
+			}
+		}
+
+	}
+	return arrayAdded;
 }
 
 var arr3 = pairwise_addition(arr1,arr1)
